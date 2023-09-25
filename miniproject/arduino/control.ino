44a41021189d4fc5c52411fb80561e6fd801d8f5
Tim Puls <timothy.puls@gmail.com>
2023-09-25 13:04:33 -0600

    rework control.ino for position

--- miniproject/arduino/control.ino
+++ miniproject/arduino/control.ino
@@ -18,39 +18,42 @@ void control_init() {
   inited = true;
 }
 
-// desired velocities
-double left_vel = 0;
-double right_vel = 0;
-// current PWM settings
+// desired positions
+int32_t left_dpos = 0;
+int32_t right_dpos = 0;
+// current PWM and dir settings
 volatile uint8_t left_pwm = 0;
+volatile bool left_dir = false;
 volatile uint8_t right_pwm = 0;
+volatile bool right_dir = false;
 // control loop, this is called by an interrupt
 void control_loop() {
-  // instead of the broken P controller we're given i'm doing an I controller i think
-  double l_error = abs(left_vel) - abs(get_vel_left()); // ignore sign when finding velocity error
-  left_pwm = max(0, min(255, left_pwm + Kp*l_error)); // pwm' = pwm + Kp*error, with bounding
-  set_dir_left(left_vel < 0); // sign matters for direction tho
+  int32_t l_error = left_dpos - get_pos_left();
+  left_pwm = max(0, min(255, Kp*abs(l_error)));
+  left_dir = l_error < 0;
+  set_dir_left(left_dir);
   set_pwm_left(left_pwm);
 
-  double r_error = abs(right_vel) - abs(get_vel_right());
-  right_pwm = max(0, min(255, right_pwm + Kp*r_error));
-  set_dir_right(right_vel < 0);
+  int32_t r_error = right_dpos - get_pos_right();
+  right_pwm = max(0, min(255, Kp*abs(r_error)));
+  right_dir = r_error < 0;
+  set_dir_right(right_dir);
   set_pwm_right(right_pwm);
 }
 // getters for current PWM and dir i guess
 uint8_t get_pwm_left() { return left_pwm; }
 uint8_t get_pwm_right() { return right_pwm; }
-bool get_dir_left() { return left_vel < 0; }
-bool get_dir_right() { return right_vel < 0; }
+bool get_dir_left() { return left_dir; }
+bool get_dir_right() { return right_dir; }
 
 
-// set velocity, cm/s
-void set_vel(double v) {
-  set_vel_left(v);
-  set_vel_right(v); 
+// set position, encoder counts
+void set_pos(int32_t p) {
+  set_pos_left(p);
+  set_pos_right(p); 
 }
-void set_vel_left(double v) { left_vel = v; }
-void set_vel_right(double v) { right_vel = v; }
+void set_pos_left(int32_t p) { left_dpos = p; }
+void set_pos_right(int32_t p) { right_dpos = p; }
 
 // ISR for control loop
 ISR(TIMER2_COMPB_vect) {
